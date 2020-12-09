e58e1b465080c20e6d994b36bdf59193b9dadf01
Tala Azrak <32012162+talaaz@users.noreply.github.com>
2020-12-09 23:34:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var myTopnav = document.getElementById("myTopnav");
+
+// Get the offset position of the navbar
+var sticky = myTopnav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    myTopnav.classList.add("sticky")
+  } else {
+    myTopnav.classList.remove("sticky");
+  }
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+wheelzoom(document.querySelector('img.zoom'), {maxZoom: 4});
